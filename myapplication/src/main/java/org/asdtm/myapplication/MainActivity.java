0f21130546eecd5e2f8b400bca0cbea331268809
Eugene <aiqcms@gmail.com>
2017-01-15 15:51:28 +0300

    Create error

--- myapplication/src/main/java/org/asdtm/myapplication/MainActivity.java
+++ myapplication/src/main/java/org/asdtm/myapplication/MainActivity.java
@@ -13,8 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreat
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
